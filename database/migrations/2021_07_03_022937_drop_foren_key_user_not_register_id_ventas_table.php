ba5347f0045c592f698a4f9a1cbb9347f53885b9
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-07-02 22:30:47 -0400

    delete foren key

--- database/migrations/2021_07_03_022937_drop_foren_key_user_not_register_id_ventas_table.php
+++ database/migrations/2021_07_03_022937_drop_foren_key_user_not_register_id_ventas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIdToVentas extends Migration
+class DropForenKeyUserNotRegisterIdVentasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,7 @@ class AddUserIdToVentas extends Migration
     public function up()
     {
         Schema::table('ventas', function (Blueprint $table) {
-            $table->bigInteger('user_id')->nullable()->after('producto_id');
-            $table->unsignedBigInteger('user_not_register_id')->nullable();
-            $table->foreign('user_not_register_id')->references('id')->on('clientes_no_registrados')->onDelete('cascade')->onUpdate('cascade');
-            
+            //
         });
     }
 
@@ -29,7 +26,7 @@ class AddUserIdToVentas extends Migration
     public function down()
     {
         Schema::table('ventas', function (Blueprint $table) {
-            //
+            $table->dropForeign('user_not_register_id');
         });
     }
 }
