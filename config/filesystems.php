d3fa1bc38995344974f5b86414ff4d385f5f9650
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-07-04 10:05:42 -0400

    s3 dc

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,17 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
         ],
+       
+        'spaces' => [
+            'driver' => 's3',
+            'root' => storage_path('app/public'),
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'url' => env('do_url'),
+        ],
 
     ],
 
